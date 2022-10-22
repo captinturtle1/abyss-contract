307649b45c608a05f229fafc1bb123fe6c11c4b4
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2022-10-21 22:01:05 -0700

    token metadata
    switched from on contract metadata to regular metadata through api

--- abyss.sol
+++ abyss.sol
@@ -8,23 +8,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract abyss is ERC721, ERC721Enumerable, Ownable {
-    string[] metadataParts = [
-        "OG",
-        "Member",
-        "OG Membership #",
-        "Expired OG Membership #",
-        "Membership #",
-        "Expired Membership #",
-        "A private collective of skilled traders and investors navigating the web3 space. OG membership passes grant access to all community benefits while active as well as additional benefits exclusive to OG holders. Each OG membership runs on a 45 day renewal system and can be managed at anytime via our dashboard. Artwork by rebelyuzz.",
-        "A private collective of skilled traders and investors navigating the web3 space. Membership passes grant access to all community benefits while active. Each membership runs on a 30 day renewal system and can be managed at anytime via our dashboard. Artwork by rebelyuzz.",
-        "https://www.abyssfnf.com/api/activeGif/",
-        "https://www.abyssfnf.com/api/expiredGif/",
-        "",
-        "https://www.abyssfnf.com/api/active/",
-        "https://www.abyssfnf.com/api/expired/",
-        ""
-    ];
-
+    string public baseURI = "https://www.abyssfnf.com/api/nft/";
     uint256 public constant ogTokenEnd = 10;
     uint256 public maxSupply = 100;
     uint256 public price = 0.08 ether;
@@ -112,10 +96,6 @@ contract abyss is ERC721, ERC721Enumerable, Ownable {
         canRenew = _state;
     }
 
-    function setMetadataParts(string[] memory metadataParts_) external onlyOwner {
-        metadataParts = metadataParts_;
-    }
-
     function setMerkleRoot(bytes32 _merkleRoot) external onlyOwner {
         merkleRoot = _merkleRoot;
     }
@@ -134,6 +114,10 @@ contract abyss is ERC721, ERC721Enumerable, Ownable {
 
     function setMaxRenewMonths(uint256 _maxRenewMonths) external onlyOwner {
         maxRenewMonths = _maxRenewMonths;
+    }
+
+    function setBaseURI(string memory baseURI_) external onlyOwner {
+        baseURI = baseURI_;
     }
 
     function ownerMint(address _receiver) external onlyOwner {
@@ -242,111 +226,13 @@ contract abyss is ERC721, ERC721Enumerable, Ownable {
         return _user == ownerOf(_tokenId) ? true : false;
     }
 
-    function getMetadata(uint256 _tokenId) internal view returns (string memory) {
-        uint256 _currentexpiryTime = expireTime[_tokenId];
-        string[7] memory parts;
-        parts[0] = ', "attributes": [{"trait_type": "Type","value": "';
-        parts[1] = _tokenId <= ogTokenEnd ? metadataParts[0] : metadataParts[1];
-        parts[2] = '"}, {"trait_type": "Expiration","value": "';
-        parts[3] = toString(_currentexpiryTime);
-        parts[4] = '"}, {"trait_type": "Expired","value": "';
-        parts[5] = _currentexpiryTime < block.timestamp ? "true" : "false";
-        parts[6] = '"}], ';
-        
-        string memory output = string(abi.encodePacked(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6]));
-        return output;
-    }
-
-    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-        string memory tokenIdString = toString(tokenId);
-        uint256 _currentexpiryTime = expireTime[tokenId];
-        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token.");
-
-        string[4] memory parts;
-        parts[0] = tokenId <= ogTokenEnd ? (_currentexpiryTime > block.timestamp ? metadataParts[2] : metadataParts[3]) : (_currentexpiryTime > block.timestamp ? metadataParts[4] : metadataParts[5]);
-        parts[1] = tokenId <= ogTokenEnd ? metadataParts[6] : metadataParts[7];
-        parts[2] = _currentexpiryTime > block.timestamp ? metadataParts[8] : metadataParts[9];
-        parts[3] = _currentexpiryTime > block.timestamp ? metadataParts[11] : metadataParts[12];
-
-        string memory s1 = Base64.encode(bytes(string(abi.encodePacked('{"name": "',parts[0],'',tokenIdString,'", "description": "',parts[1],'"', getMetadata(tokenId), '"'))));
-        string memory s2 = Base64.encode(bytes(string(abi.encodePacked('image": "',parts[2],'',tokenIdString,'',metadataParts[10],'", "animation_url": "',parts[3],'',tokenIdString,'',metadataParts[13],'"}'))));
-        string memory json = string(abi.encodePacked('data:application/json;base64,', string.concat(s1, s2)));
-        return json;
-    }
-
-    function toString(uint256 value) internal pure returns (string memory) {
-        if (value == 0) {
-            return "0";
-        }
-        uint256 temp = value;
-        uint256 digits;
-        while (temp != 0) {
-            digits++;
-            temp /= 10;
-        }
-        bytes memory buffer = new bytes(digits);
-        while (value != 0) {
-            digits -= 1;
-            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
-            value /= 10;
-        }
-        return string(buffer);
-    }
-}
-
-library Base64 {
-    bytes internal constant TABLE = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-
-    /// @notice Encodes some bytes to the base64 representation
-    function encode(bytes memory data) internal pure returns (string memory) {
-        uint256 len = data.length;
-        if (len == 0) return "";
-
-        // multiply by 4/3 rounded up
-        uint256 encodedLen = 4 * ((len + 2) / 3);
-
-        // Add some extra buffer at the end
-        bytes memory result = new bytes(encodedLen + 32);
-
-        bytes memory table = TABLE;
-
-        assembly {
-            let tablePtr := add(table, 1)
-            let resultPtr := add(result, 32)
-
-            for {
-                let i := 0
-            } lt(i, len) {
-
-            } {
-                i := add(i, 3)
-                let input := and(mload(add(data, i)), 0xffffff)
-
-                let out := mload(add(tablePtr, and(shr(18, input), 0x3F)))
-                out := shl(8, out)
-                out := add(out, and(mload(add(tablePtr, and(shr(12, input), 0x3F))), 0xFF))
-                out := shl(8, out)
-                out := add(out, and(mload(add(tablePtr, and(shr(6, input), 0x3F))), 0xFF))
-                out := shl(8, out)
-                out := add(out, and(mload(add(tablePtr, and(input, 0x3F))), 0xFF))
-                out := shl(224, out)
-
-                mstore(resultPtr, out)
-
-                resultPtr := add(resultPtr, 4)
-            }
-
-            switch mod(len, 3)
-            case 1 {
-                mstore(sub(resultPtr, 2), shl(240, 0x3d3d))
-            }
-            case 2 {
-                mstore(sub(resultPtr, 1), shl(248, 0x3d))
-            }
-
-            mstore(result, encodedLen)
-        }
-
-        return string(result);
+    function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+        require(_exists(_tokenId), "Token does not exist.");
+        return bytes(baseURI).length > 0 ? string(
+            abi.encodePacked(
+              baseURI,
+              Strings.toString(_tokenId)
+            )
+        ) : "";
     }
 }
